02601cd958c707e2afc44f0e96ef1f1cb8bfd7d9
sav <sav.acharya22@gmail.com>
2023-12-09 11:32:17 +1100

    view todo

--- todo_functions.py
+++ todo_functions.py
@@ -16,6 +16,7 @@ def add_todo(file_name):
 def remove_todo(file_name):
     print("remove to do")
     
+    
 
 
 def mark_todo(file_name):
@@ -25,5 +26,13 @@ def mark_todo(file_name):
 
 def view_todo(file_name):
     print("view to do")
+    with open(file_name, "r") as f:
+        reader = csv.reader(f)
+        reader.__next__()
+        for row in reader:
+            if (row[1] == "True"):
+                print(f"Todo {row[0]} is completed")
+            else:
+                print(f"Todo {row[0]} is not complete")
     
     
